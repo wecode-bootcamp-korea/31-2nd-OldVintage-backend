5325b0f0a2c2dca47d7b19d6560ada169130db52
angrybirdpark <thesmileless90@gmail.com>
2022-04-13 17:59:48 +0900

    Fix: modeling

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ class User(TimestampZone):
     email      = models.CharField(max_length=100, unique=True)
     password   = models.CharField(max_length=200)
     name       = models.CharField(max_length=100, null=True)
-    kakao_id   = models.IntegerField()
+    kakao_id   = models.IntegerField(null=True)
 
     class Meta:
         db_table = 'users'
